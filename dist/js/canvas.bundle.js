81dc8885e53feb64a381cf618aabf5b5bd40e44e
kdub5184 <kdub5184@gmail.com>
2022-03-26 00:29:18 -0400

    platform tile set
    got server working with the platform tile set

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -86,6 +86,19 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/img/iceplatform.png":
+/*!*********************************!*\
+  !*** ./src/img/iceplatform.png ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "beedd89027522de93f7424558f5151d1.png");
+
+/***/ }),
+
 /***/ "./src/js/canvas.js":
 /*!**************************!*\
   !*** ./src/js/canvas.js ***!
@@ -95,8 +108,7 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _img_iceplatform_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../img/iceplatform.png */ "./src/img/iceplatform.png");
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -104,105 +116,234 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 
+console.log(_img_iceplatform_png__WEBPACK_IMPORTED_MODULE_0__["default"]);
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-var mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
-};
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']; // Event Listeners
+var gravity = 0.6;
 
-addEventListener('mousemove', function (event) {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-});
-addEventListener('resize', function () {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-  init();
-}); // Objects
+var Player = /*#__PURE__*/function () {
+  function Player() {
+    _classCallCheck(this, Player);
 
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
-
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
+    this.position = {
+      x: 100,
+      y: 100
+    };
+    this.velocity = {
+      x: 0,
+      y: 1
+    };
+    this.width = 30;
+    this.height = 30;
   }
 
-  _createClass(Object, [{
+  _createClass(Player, [{
     key: "draw",
     value: function draw() {
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
-      c.fill();
-      c.closePath();
+      c.fillStyle = 'blue';
+      c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
   }, {
     key: "update",
     value: function update() {
       this.draw();
+      this.position.y += this.velocity.y;
+      this.position.x += this.velocity.x;
+      if (this.position.y + this.height + this.velocity.y <= canvas.height) this.velocity.y += gravity;else this.velocity.y = 0;
     }
   }]);
 
-  return Object;
-}(); // Implementation
+  return Player;
+}();
 
+var Platform = /*#__PURE__*/function () {
+  function Platform(_ref) {
+    var x = _ref.x,
+        y = _ref.y,
+        image = _ref.image;
 
-var objects;
+    _classCallCheck(this, Platform);
 
-function init() {
-  objects = [];
-
-  for (var i = 0; i < 400; i++) {// objects.push()
+    this.position = {
+      x: x,
+      y: y,
+      image: ''
+    };
+    this.image = image;
+    this.width = this.image.width;
+    this.height = this.image.height;
   }
-} // Animation Loop
 
+  _createClass(Platform, [{
+    key: "draw",
+    value: function draw() {
+      c.drawImage(this.image, this.position.x, this.position.y); //c.fillStyle = 'green'
+      //c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+  }]);
 
-function animate() {
+  return Platform;
+}();
+
+var image = new Image();
+image.src = _img_iceplatform_png__WEBPACK_IMPORTED_MODULE_0__["default"];
+console.log(image);
+var player = new Player();
+var platforms = [new Platform({
+  x: 0,
+  y: canvas.height - image.height,
+  image: image
+}), new Platform({
+  x: 350,
+  y: canvas.height - 250,
+  image: image
+}), new Platform({
+  x: 800,
+  y: canvas.height - 450,
+  image: image
+})];
+var keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  },
+  jump: {
+    pressed: false
+  }
+};
+var myGamepad;
+var counter;
+var scrollOffset = 0;
+
+function animate(counter) {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y); // objects.forEach(object => {
-  //  object.update()
-  // })
+  player.update();
+  platforms.forEach(function (platform) {
+    platform.draw();
+  }); //myGamepad = navigator.getGamepads()[0]; // use the first gamepad (xbox-only)
+  //keyboard nav
+
+  if (keys.right.pressed && player.position.x < 500) {
+    console.log('move right');
+    player.velocity.x = 5;
+  } else if (keys.left.pressed && player.position.x > 100) {
+    console.log("move left");
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+
+    if (keys.right.pressed) {
+      platforms.forEach(function (platform) {
+        platform.position.x -= 5;
+        scrollOffset += 5;
+      });
+    } else if (keys.left.pressed) {
+      platforms.forEach(function (platform) {
+        platform.position.x += 5;
+        scrollOffset -= 5;
+      });
+    }
+  }
+
+  if (scrollOffset > 3000) {
+    player.velocity.x = 0;
+    console.log("you win");
+  } //Game pad code
+  // if (  && player.position.x < 500) {
+  //     console.log('move right')
+  //     player.velocity.x = 5
+  // } else if(myGamepad.axes[0] <= -.8 && player.position.x > 100) {
+  //     player.velocity.x = -5
+  // } else {
+  //     player.velocity.x = 0
+  // if(myGamepad.axes[0] > .8){
+  //     platforms.forEach(platform => {
+  //         platform.position.x -= 5
+  //     })
+  // }else if (myGamepad.axes[0] <= -.8){
+  //     platforms.forEach(platform => {
+  //         platform.position.x += 5
+  //     })
+  // }
+  // }
+  //gamepad jump function
+  // if (myGamepad.buttons[0].pressed){
+  //     console.log("jump")
+  //     console.log(myGamepad.buttons[0].pressed)
+  //         player.velocity.y = -15
+  // }else{
+  //     console.log("do you get here")
+  //     myGamepad.buttons[0].pressed = false
+  // }
+  //tracking collion on platform
+
+
+  platforms.forEach(function (platform) {
+    if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+      player.velocity.y = 0;
+    }
+  });
+  console.log("animation loop running");
 }
 
-init();
 animate();
+addEventListener('keydown', function (_ref2) {
+  var keyCode = _ref2.keyCode;
 
-/***/ }),
+  //console.log(keyCode)
+  switch (keyCode) {
+    case 65:
+      console.log(" pressing left button");
+      keys.left.pressed = true;
+      break;
 
-/***/ "./src/js/utils.js":
-/*!*************************!*\
-  !*** ./src/js/utils.js ***!
-  \*************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+    case 83:
+      console.log("down");
+      player.velocity.y = 0;
+      break;
 
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+    case 68:
+      console.log("right");
+      keys.right.pressed = true;
+      break;
 
-function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
+    case 87:
+      console.log("up");
+      player.velocity.y -= 25;
+      break;
+  }
+});
+addEventListener('keyup', function (_ref3) {
+  var keyCode = _ref3.keyCode;
 
-function distance(x1, y1, x2, y2) {
-  var xDist = x2 - x1;
-  var yDist = y2 - y1;
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-}
+  //console.log(keyCode)
+  switch (keyCode) {
+    case 65:
+      console.log("left");
+      keys.left.pressed = false;
+      break;
 
-module.exports = {
-  randomIntFromRange: randomIntFromRange,
-  randomColor: randomColor,
-  distance: distance
-};
+    case 83:
+      console.log("down");
+      player.velocity.y = 0;
+      break;
+
+    case 68:
+      console.log("right");
+      keys.right.pressed = false;
+      break;
+
+    case 87:
+      console.log("up");
+      player.velocity.y = 0;
+      break;
+  }
+});
 
 /***/ })
 
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,4 +1,5 @@
-import utils from './utils'
+import platform from '../img/iceplatform.png'
+console.log(platform)
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
@@ -6,68 +7,217 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
+const gravity = 0.6
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+
+        this.width = 30
+        this.height = 30
+    }
+
+    draw(){
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
+        
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+        this.velocity.y += gravity
+        else this.velocity.y = 0
+        
+    }
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+class Platform {
+    constructor({x,y, image}) {
+        this.position = {
+            x: x,
+            y: y,
+            image: ''
+        }
 
-// Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
+        this.image = image
+        this.width = this.image.width
+        this.height = this.image.height
+    }
 
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
-
-  init()
-})
-
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
-
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
-
-  update() {
-    this.draw()
-  }
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+        //c.fillStyle = 'green'
+        //c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
+const image = new Image()
+image.src = platform
+console.log(image)
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
-  }
+const player = new Player()
+const platforms = [new Platform({x:0, y:canvas.height - image.height, image: image}),
+                    new Platform({x: 350, y:canvas.height - 250, image: image}),
+                    new Platform({x: 800, y:canvas.height - 450, image: image})
+                ]
+
+const keys = {
+    right:{
+        pressed: false
+    },
+    left:{
+        pressed: false
+    },
+    jump:{
+        pressed:false
+    }
 }
 
-// Animation Loop
-function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+let myGamepad
+var counter
+let scrollOffset = 0
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+function animate(counter, ){
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    platforms.forEach(platform => {
+        platform.draw()
+    })
+    
+    
+    //myGamepad = navigator.getGamepads()[0]; // use the first gamepad (xbox-only)
+    
+    //keyboard nav
+    if (keys.right.pressed && player.position.x < 500) {
+        console.log('move right')
+        player.velocity.x = 5
+    } else if(keys.left.pressed && player.position.x > 100) {
+        console.log("move left")
+        player.velocity.x = -5
+    } else {
+        player.velocity.x = 0
+
+        if(keys.right.pressed){
+            
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+                scrollOffset += 5
+            })
+        }else if (keys.left.pressed){
+            
+            platforms.forEach(platform => {
+                platform.position.x += 5
+                scrollOffset -= 5
+            })
+        }
+
+    }
+
+    if(scrollOffset > 3000){
+        player.velocity.x = 0
+        console.log("you win")
+    }
+
+    
+
+    //Game pad code
+    // if (  && player.position.x < 500) {
+    //     console.log('move right')
+    //     player.velocity.x = 5
+    // } else if(myGamepad.axes[0] <= -.8 && player.position.x > 100) {
+    //     player.velocity.x = -5
+    // } else {
+    //     player.velocity.x = 0
+
+                // if(myGamepad.axes[0] > .8){
+                        
+                //     platforms.forEach(platform => {
+                //         platform.position.x -= 5
+                //     })
+                // }else if (myGamepad.axes[0] <= -.8){
+                    
+                //     platforms.forEach(platform => {
+                //         platform.position.x += 5
+                //     })
+                // }
+    // }
+
+    //gamepad jump function
+    // if (myGamepad.buttons[0].pressed){
+    //     console.log("jump")
+    //     console.log(myGamepad.buttons[0].pressed)
+    //         player.velocity.y = -15
+    // }else{
+    //     console.log("do you get here")
+    //     myGamepad.buttons[0].pressed = false
+    // }
+
+    //tracking collion on platform
+
+    platforms.forEach(platform => {
+        if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width){
+            player.velocity.y = 0
+        }
+    })
+
+    console.log("animation loop running")
 }
 
-init()
 animate()
+
+addEventListener('keydown', ({keyCode}) => {
+    //console.log(keyCode)
+    switch (keyCode) {
+        case 65:
+            console.log(" pressing left button")
+            keys.left.pressed = true
+            break
+        case 83:
+            console.log("down")
+            player.velocity.y = 0
+            break
+        case 68:
+            console.log("right")
+            keys.right.pressed = true
+            break
+        case 87:
+            console.log("up")
+            player.velocity.y -= 25
+            break
+    }
+})
+
+addEventListener('keyup', ({keyCode}) => {
+    //console.log(keyCode)
+    switch (keyCode) {
+        case 65:
+            console.log("left")
+            keys.left.pressed = false
+            break
+        case 83:
+            console.log("down")
+            player.velocity.y = 0
+            break
+        case 68:
+            console.log("right")
+            keys.right.pressed = false
+            break
+        case 87:
+            console.log("up")
+            player.velocity.y = 0
+            break
+    }
+})
+
